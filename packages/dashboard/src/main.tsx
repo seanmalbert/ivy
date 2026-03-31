e8464c4b431bc8e2798f92f08067622d2636dad9
Sean Albert <smabert@gmail.com>
2026-03-31 09:45:19 -0700

    adds icons

--- packages/dashboard/src/main.tsx
+++ packages/dashboard/src/main.tsx
@@ -5,6 +5,7 @@ import { Layout } from "./components/Layout";
 import { DomainPicker } from "./pages/DomainPicker";
 import { DomainOverview } from "./pages/DomainOverview";
 import { PageDetail } from "./pages/PageDetail";
+import { Privacy } from "./pages/Privacy";
 import "./app.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
           <Route index element={<DomainPicker />} />
           <Route path=":domain" element={<DomainOverview />} />
           <Route path=":domain/page" element={<PageDetail />} />
+          <Route path="privacy" element={<Privacy />} />
         </Route>
       </Routes>
     </BrowserRouter>
